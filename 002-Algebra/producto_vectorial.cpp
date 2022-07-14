4f389516b585d301a995ac4e971e3491c5dbd604
Isaias Gonzalez <isaiasegz@pm.me>
2022-07-14 03:17:43 -0300

    002-algebra

---
+++ 002-Algebra/producto_vectorial.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <math.h>
+
+int main() {
+
+    float ax, ay, az, bx, by, bz, rx, ry, rz;
+
+    std::cout << "ax: ";
+    std::cin >> ax;
+    std::cout << "ay: ";
+    std::cin >> ay;
+    std::cout << "az: ";
+    std::cin >> az;
+
+    std::cout << "bx: ";
+    std::cin >> bx;
+    std::cout << "by: ";
+    std::cin >> by;
+    std::cout << "bz: ";
+    std::cin >> bz;
+
+    rx = ( ay * bz - az * by );
+    ry = ( az * bx - ax * bz );
+    rz = ( ax * by - ay * bx );
+
+    std::cout << "(" << rx << "; " << ry << "; " << rz << ")";
+
+    return 0;
+}
